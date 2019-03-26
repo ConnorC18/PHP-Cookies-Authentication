61e1124931fb4ada7f37167dd8150df0f17786a3
ConnorC18 <connor.coleman2002@gmail.com>
2019-03-26 20:24:00 +0000

    V 1.0.0

---
+++ includes/401.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>We've got some trouble | 401 - Unauthorized</title>
+    <link rel="stylesheet" href="./includes/assets/css/401.css">
+</head>
+<body>
+    <div class="container">
+		<div class="forbidden-sign"></div>
+		<h1>Access to this page is restricted.</h1>
+		<p>Ensure you have sufficient permissions to access the page.</p>
+	</div>
+</body>
+</html>
